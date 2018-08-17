adf6ca3667fcd78032ad1b39a8ab661545bc6d47
CN02233 <black41802935@163.com>
2018-08-17 09:43:21 +0800

    add vue shower

---
+++ vue-shower/src/models/login.vue
@@ -0,0 +1,157 @@
+<template>
+    <div class="login-container">
+        <el-form class="login-form" autoComplete="on" ref="loginForm" label-position="left">
+            <h3 class="title">欢迎！</h3>
+            <el-form-item prop="user_name">
+                <el-input name="user_name" type="text" v-model="loginForm.user_name" autoComplete="on"
+                          placeholder="user_name"/>
+            </el-form-item>
+            <el-form-item prop="user_pwd">
+                <el-input name="user_pwd" :type="pwdType" @keyup.enter.native="handleLogin" v-model="loginForm.user_pwd"
+                          autoComplete="on"
+                          placeholder="user_pwd"></el-input>
+                <!--<span class="show-pwd" @click="showPwd"><svg-icon icon-class="eye" /></span>-->
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" style="width:100%;" :loading="loading" @click.native.prevent="handleLogin">
+                    <!--<el-button type="primary" style="width:100%;" :loading="loading">-->
+                    登入系统
+                </el-button>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data() {
+    return {
+        loginForm: {
+            user_name: 'admin',
+            user_pwd: 'admin'
+        },
+        loading: false,
+        pwdType: 'user_pwd'
+    }
+  },
+  methods: {
+    showPwd() {
+        if (this.pwdType === 'user_pwd') {
+            this.pwdType = ''
+        } else {
+            this.pwdType = 'user_pwd'
+        }
+    },
+    handleLogin() {
+        console.log("handle login is running.....");
+        this.$router.push({path: '/infoInput'});
+//                this.$refs.loginForm.validate(valid => {
+//                    if (valid) {
+//                        this.loading = true
+//                        this.$store.dispatch('Login', this.loginForm).then(() => {
+//                            this.loading = false
+//                        this.$router.push({ path: '/' })
+//                    }).catch(() => {
+//                            this.loading = false
+//                    })
+//                    } else {
+//                        console.log('error submit!!')
+//                    return false
+//                }
+//            })
+    }
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+    $bg: #2d3a4b;
+    $light_gray: #eee;
+
+    /* reset element-ui css */
+    .login-container {
+        .el-input {
+            display: inline-block;
+            height: 47px;
+            width: 85%;
+            input {
+                background: transparent;
+                border: 0px;
+                -webkit-appearance: none;
+                border-radius: 0px;
+                padding: 12px 5px 12px 15px;
+                color: $light_gray;
+                height: 47px;
+                &:-webkit-autofill {
+                    -webkit-box-shadow: 0 0 0px 1000px $bg inset !important;
+                    -webkit-text-fill-color: #fff !important;
+                }
+            }
+        }
+        .el-form-item {
+            border: 1px solid rgba(255, 255, 255, 0.1);
+            background: rgba(0, 0, 0, 0.1);
+            border-radius: 5px;
+            color: #454545;
+        }
+    }
+
+</style>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+    $bg: #2d3a4b;
+    $dark_gray: #889aa4;
+    $light_gray: #eee;
+    .login-container {
+        position: fixed;
+        height: 100%;
+        width: 100%;
+        background-color: $bg;
+        .login-form {
+            position: absolute;
+            left: 0;
+            right: 0;
+            width: 520px;
+            padding: 35px 35px 15px 35px;
+            margin: 120px auto;
+        }
+        .tips {
+            font-size: 14px;
+            color: #fff;
+            margin-bottom: 10px;
+            span {
+                &:first-of-type {
+                    margin-right: 16px;
+                }
+            }
+        }
+        .svg-container {
+            padding: 6px 5px 6px 15px;
+            color: $dark_gray;
+            vertical-align: middle;
+            width: 30px;
+            display: inline-block;
+            &_login {
+                font-size: 20px;
+            }
+        }
+        .title {
+            font-size: 26px;
+            font-weight: 400;
+            color: $light_gray;
+            margin: 0px auto 40px auto;
+            text-align: center;
+            font-weight: bold;
+        }
+        .show-pwd {
+            position: absolute;
+            right: 10px;
+            top: 7px;
+            font-size: 16px;
+            color: $dark_gray;
+            cursor: pointer;
+            user-select: none;
+        }
+    }
+</style>
---
+++ vue-shower/src/models/menu/top-menu/src/menu.vue
@@ -0,0 +1,63 @@
+<template>
+    <router-link :to="menuClick()">
+      <div class="work-menu">
+        {{menu.name}}
+      </div>
+    </router-link>
+
+
+</template>
+
+<script>
+  export default {
+    name: 'WorkTopMenu',
+    data() {
+        return {}
+    },
+    props:{
+      menu:{
+        type: Object
+      }
+    },
+    methods: {
+      hasChildren:function(){
+        let hasChildren = false;
+        if(this.menu.children&&this.menu.children.length>0){
+          hasChildren = true;
+        }
+        this.menu.hasChildren = hasChildren;
+        return hasChildren;
+      },
+      menuClick:function(){
+        if(!this.menu.hasChildren){
+          this.menu.hasClick = true;
+          console.log(this.menu.name)
+          return this.menu.url;
+        }
+      }
+    }
+  }
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+
+  .work-menu{
+    height:100%;
+    margin:0 10px 0 10px;
+    padding:0 10px 0 10px;
+    color:#ffffff;
+    font-size:16px ;
+    float: left;
+    cursor: pointer;
+    min-width: 100px;
+    text-align: center;
+    &:hover{
+      background-color: #0000fe;
+    }
+  }
+
+  .work-menu-click{
+    background-color: #0000fe;
+  }
+
+</style>
